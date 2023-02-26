f78c1a702ab6c0c51ed0c0a7d6fb7f65ea324c6e
Felipe Carlos Werlang <felipewer@gmail.com>
2023-02-26 17:42:32 -0300

    Add CORS headers

--- functions/[compressed].ts
+++ functions/[compressed].ts
@@ -1,12 +1,23 @@
 import * as LZString from 'lz-string'
 
+export const onRequestOptions: PagesFunction<{}> = async (context) => {
+  return new Response(null,{
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS'
+    }
+  })
+}
+
 export const onRequestGet: PagesFunction<{}> = async (context) => {
   try {
     const encoded = context.functionPath.slice(1)
     const decoded = LZString.decompressFromEncodedURIComponent(encoded)
   
     return new Response(decoded, { headers: {
-      'Content-Type': 'application/json;charset=utf-8'
+      'Content-Type': 'application/json;charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods':'GET,HEAD,OPTIONS'
     }});
   } catch (err) {
     console.error(err)
